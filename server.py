171ad4591c33d4e75fb59d412a5d91d0da749cbf
Akila Welihinda <akila@Akilas-MacBook-Pro.local>
2016-07-09 15:10:26 -0700

    sql test

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
 
 @app.route('/login', methods=['POST'])
 def login():
 	email=request.form['email']
-	
-
 
 
 if __name__ == "__main__":
--- src/getPlaces.py
+++ src/getPlaces.py
@@ -2,6 +2,7 @@ import io
 from yelpapi import YelpAPI
 from geopy.geocoders import Nominatim
 import json
+import MySQLdb
 
 yelp_api = YelpAPI('rRkbtARpvQcSL3l-ZRIN0w',
     'G3KrPharmO7HX1FiZ0ApyrZll2Q',
@@ -23,11 +24,22 @@ print address
 search_results = yelp_api.search_query(term="tourist attractions",
     location=address, limit=20, sort=2, radius_filter=8000)
 
-print json.dumps(search_results, sort_keys=True, indent=4)
+return search_results
+
+#print json.dumps(search_results, sort_keys=True, indent=4)
+
+#connect to db
+db = MySQLdb.connect("localhost","root","greylock","greylock" )
+cursor = db.cursor()
+cursor.execute("SELECT VERSION()")
+data = cursor.fetchone()
+print "Database version : %s " % data
 
 for biz in search_results['businesses']:
-  print biz['name']
-  print biz['image_url']
-  print biz['rating']
-  print biz['location']['coordinate']['latitude']
-  print biz['location']['coordinate']['longitude']
+  if biz['rating'] >= 3.5:
+    print biz['name']
+    print biz['image_url']
+    print biz['rating']
+    print biz['location']['coordinate']['latitude']
+    print biz['location']['coordinate']['longitude']
+
