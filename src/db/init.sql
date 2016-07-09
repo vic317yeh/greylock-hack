79c86e20b56bbd03008c5401e80fe43d9a794a47
Vic Yeh <vic317_yeh@hotmail.com>
2016-07-09 22:34:10 +0000

    modify init.sql

--- src/db/init.sql
+++ src/db/init.sql
@@ -18,6 +18,7 @@ CREATE TABLE Friends (
 
 CREATE TABLE Flags (
 	fid INT NOT NULL AUTO_INCREMENT,
+	name TEXT NOT NULL,
 	location_lat DECIMAL(10, 8) NOT NULL,
 	location_log DECIMAL(11, 8) NOT NULL,	
 	rating INT NOT NULL,
