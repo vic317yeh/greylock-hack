c01984e122400f4affe893dd6296be9ffbaeb845
Vic Yeh <vic317_yeh@hotmail.com>
2016-07-09 19:54:06 -0700

    raw

--- src/match.py
+++ src/match.py
@@ -5,15 +5,10 @@ import os
 DIFFERENCE_BAR = 10
 
 def match(userImgSrc, targetImgSrc)
-	basedir = os.path.dirname(os.path.realpath(__file__))
-	userImgSrc = '6.JPG'
-	targetImgSrc = '7.JPG'
-	user_img = Image.open(basedir + '/../static/img/' + userImgSrc)
+	user_img = Image.open(userImgSrc)
+	target_img = Image.open(targetImgSrc)
 	user_hash = imagehash.average_hash(user_img)
-	target_img = Image.open(basedir + '/../static/img/' + targetImgSrc)
 	target_hash = imagehash.average_hash(target_img)
+	result = True if user_hash-target_hash <= DIFFERENCE_BAR else False
+	return result
 
-	if user_hash-target_hash <= DIFFERENCE_BAR:
-		return True
-	else:
-		return False
