b16b38dbd8ffbe4b30390f963c0a0dc039b039a4
Vic Yeh <vic317_yeh@hotmail.com>
2016-07-09 19:51:49 -0700

    yo

--- src/match.py
+++ src/match.py
@@ -1,7 +1,19 @@
 from PIL import Image
 import imagehash
+import os
 
-user_url = ''
-user_hash = imagehash.pHash(Image.open(user_url))
-target_url = ''
-target_hash = imagehash.pHash(Image.open(target_url))
+DIFFERENCE_BAR = 10
+
+def match(userImgSrc, targetImgSrc)
+	basedir = os.path.dirname(os.path.realpath(__file__))
+	userImgSrc = '6.JPG'
+	targetImgSrc = '7.JPG'
+	user_img = Image.open(basedir + '/../static/img/' + userImgSrc)
+	user_hash = imagehash.average_hash(user_img)
+	target_img = Image.open(basedir + '/../static/img/' + targetImgSrc)
+	target_hash = imagehash.average_hash(target_img)
+
+	if user_hash-target_hash <= DIFFERENCE_BAR:
+		return True
+	else:
+		return False
